8d1db967d15312e714eda1ed42de2093b5a77dd5
Davidedl2001 <davidedeleonardis1711@gmail.com>
2022-01-08 18:57:09 +0100

    add switch theme button complete

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@
     // Dare la possibilità all'utente di cancellare tutti i messaggi di un contatto o di cancellare l'intera chat con tutti i suoi dati: cliccando sull'icona con i tre pallini in alto a destra, si apre un dropdown menu in cui sono presenti le voci "Elimina messaggi" ed "Elimina chat"; cliccando su di essi si cancellano rispettivamente tutti i messaggi di quel contatto (quindi rimane la conversazione vuota) oppure l'intera chat comprensiva di tutti i dati del contatto oltre che tutti i suoi messaggi (quindi sparisce il contatto anche dalla lista di sinistra)
     // Visualizzare un messaggio di benvenuto che invita l'utente a selezionare un contatto dalla lista per visualizzare i suoi messaggi, anziché attivare di default la prima conversazione
         // Aggiungere una splash page visibile per 1s all'apertura dell'app
-    // Aggiungere un'icona per cambiare la modalità light/dark
+        // Aggiungere un'icona per cambiare la modalità light/dark
         // Aggiungere un'icona per ingrandire o rimpicciolire il font
 
 const app = new Vue(
@@ -262,7 +262,7 @@ const app = new Vue(
         created() {
             setTimeout(() => {
                 this.varSplash = true;
-            }, 1500);
+            }, 0);
             this.contacts[this.counter].lastAccess = this.getLastAccess();
                 // lastAccess si vede all'apertura solo in contacts[0]
         }
